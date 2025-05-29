dec6877b922be1cef665a8fd64254f7ec8c94a03
geeth <geeth@efuturesworld.com>
2025-05-29 20:22:22 +0530

    my chages added add hpa

---
+++ infrastructure/eks-cluster/scripts/configure-bastion.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+#install kubectl lastest version -> ubuntu
+#curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+
+#install kubectl specific version
+curl -LO https://dl.k8s.io/release/v1.33.0/bin/linux/amd64/kubectl
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+
